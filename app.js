a8a65f6d397592fb6ddab6c50771ccd15fb898ad
Yash Thaker <yasht.spaceo@gmail.com>
2022-07-12 19:11:10 +0530

    Refactor code

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ fs.createReadStream('contacts.csv')
     // console.log(contacts);
 });
 
-// let counter = { fails: 0, success: 0 }
+let counter = { fails: 0, success: 0 }
 
 /*
 const SESSION_FILE_PATH = './whatsapp-session.json';
@@ -70,23 +70,41 @@ client.on('disconnected', (reason) => {
     console.log('Client was logged out', reason);
 });
 
-
-function deploy_all() {   
-
+async function deploy_all() {
     for (const contact of contacts) {
         const final_number = (contact.length > 10) ? `${contact}@c.us` : `91${contact}@c.us`;
-        
-        client.isRegisteredUser(final_number).then(function(isRegistered) {
-            if(isRegistered) {
-                client.sendMessage(final_number, message).then(function(msg) {
-                		console.log(`${contact} Sent`);
-                		// counter.success++;
-                	});
-            } else {
-                console.log(`${contact} Failed`);
-                // counter.fails++;
-            }
-        });
+        const isRegistered = await client.isRegisteredUser(final_number);
+        if (isRegistered) {
+            const msg = await client.sendMessage(final_number, message);
+            console.log(`${contact} Sent`);
+            counter.success++;
+            /*
+            deleteChat(final_number)
+            .then((res) => console.log(res)) // contains ["successfuly deleted"]
+            .catch((err) => console.log(err))// contains ["something went wrong", "do not have chat history"]
+            */
+        } else {
+            console.log(`${contact} Failed`);
+            counter.fails++;
+        }
     }
-    // console.log(`Result: ${counter.success} sent, ${counter.fails} failed`);
+    console.log(`Result: ${counter.success} sent, ${counter.fails} failed`);
+}
+
+async function deleteChat(phoneNumber) {
+    return new Promise((resolve, reject) => {
+        client.getChatById(phoneNumber).then((chat) => {
+            // console.log("Chat information = ", chat)
+            chat.delete().then((deleteRes) => {
+                if(deleteRes) 
+                    resolve(`successfuly deleted`)
+                else 
+                    reject("something went wrong")
+            })
+        }).catch((err) => {
+            if(err.message.includes("Cannot read property 'serialize' of undefined"))
+                reject(`do not have chat history`)
+            // can handle other error messages...     
+        })
+    })
 }
